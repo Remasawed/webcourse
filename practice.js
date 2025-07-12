bf75f30e937c2a71ac2b24d8ebe774344b1372d4
Remasawed <remasawed2008@gmail.com>
2025-07-12 10:14:51 +0300

    new work

--- practice.js
+++ practice.js
@@ -1,5 +1,14 @@
-function toggleShare() {
-  const icons = document.getElementById("socialIcons");
-  icons.style.display = icons.style.display === "flex" ? "none" : "flex";
-  icons.style.flexDirection = "row";
+let sizeRadios = document.getElementsByName('size');
+let sizePrice = 0;
+
+for (let radio of sizeRadios) {
+  if (radio.checked) {
+    sizePrice = parseFloat(radio.value);
+    break;
+  }
 }
+let toppings = document.querySelectorAll('input[type="checkbox"]:checked');
+let toppingsPrice = toppings.length * 2;
+let total = sizePrice + toppingsPrice;
+document.getElementById('total').textContent = "Total: $" + total;
+<p id="total">Total: $19</p>
